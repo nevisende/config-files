16b327b09972b855b8b1cbd5f770bc2144f6f8d5
nevisende <ekonomitakipmail@gmail.com>
2022-04-24 01:20:13 +0300

    feat: add eslint file

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+    node: true,
+  },
+  "parser": "babel-eslint",
+  "parserOptions": {
+    "ecmaVersion": 2018,
+    "sourceType": "module"
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  rules: {
+    semi: 0,
+    'no-console': 0,
+  },
+};
